b0215748b9803ab59e9324820a99751daace7164
Kaustubh Patange <DeveloperKP16@gmail.com>
2023-03-29 15:35:58 +0530

    add: spotless hook in :build & pre-commit hook (#1)

--- flare/src/main/java/io/pantheon/flare/AnalyticsGenerator.kt
+++ flare/src/main/java/io/pantheon/flare/AnalyticsGenerator.kt
@@ -1,32 +1,48 @@
+/**
+ * Copyright 2023 LaatonWalaBhoot
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.pantheon.flare
 
-import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.TypeSpec
 import java.io.File
 
 fun generateEventPropertyMappings() {
     // using kotlinpoet here
 
-    val typeBuilder =  TypeSpec.classBuilder("AnalyticsPayload")
+    val typeBuilder = TypeSpec.classBuilder("AnalyticsPayload")
         .addModifiers(KModifier.DATA)
 
     val funSpecBuilder = FunSpec.constructorBuilder()
 
-    constants.forEach {
-        funSpecBuilder.addParameter(it.snakeToCamelCase(), String::class)
-        typeBuilder.addProperty(PropertySpec.builder(it.snakeToCamelCase(), String::class)
-            .initializer(it.snakeToCamelCase())
-            .build())
-    }
+//    constants.forEach {
+//        funSpecBuilder.addParameter(it.snakeToCamelCase(), String::class)
+//        typeBuilder.addProperty(PropertySpec.builder(it.snakeToCamelCase(), String::class)
+//            .initializer(it.snakeToCamelCase())
+//            .build())
+//    }
 
     FileSpec.builder("", "AnalyticsPayload")
         .addType(typeBuilder.primaryConstructor(funSpecBuilder.build()).build())
         .build()
         .writeTo(File("flare/src/main/java/io/pantheon/flare"))
-
 }
 
 fun String.snakeToCamelCase(): String {
     val pattern = "_[a-z]".toRegex()
     return replace(pattern) { it.value.last().uppercase() }
 }
-
